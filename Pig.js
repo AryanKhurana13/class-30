48fe8a2539a69d31b087f677c8067c0ffa2a06b3
Aryan <aryanskhurana@gmail.com>
2020-09-16 20:07:12 +0530

    vp

--- Pig.js
+++ Pig.js
@@ -2,6 +2,22 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visible = 255;
   }
-
+  display(){
+    console.log(this.body.speed);
+    if (this.body.speed<3){
+      //display the pigs
+      super.display();
+    }
+    else{
+      //vanish the pigs
+        World.remove(world,this.body);
+        push();
+        this.visible=this.visible-5;
+        tint(255,this.visible)
+        image(this.image,this.body.position.x,this.body.position.y,50,50)
+        pop();
+    }
+  }
 };
